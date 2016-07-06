4584018866a2a98e52e9763dce2d350b34ce15a8
Yaowenjie <wjyao@thoughtworks.com>
2016-07-06 12:23:22 +0800

    Completed the available version

--- chart.js
+++ chart.js
@@ -20,7 +20,6 @@ if (overallDiv.length !== 0) {
   chartDiv.setAttribute("id", "chartContainer");
   chartDiv.style.cssText = 'height: 300px; width: 100%; border: 1px solid #ddd;border-radius: 0 3px 3px 0;';
 
-  overallDiv[0].parentNode.insertBefore(chartDiv, overallDiv[0].nextSibling);
   /* Getting Data from github page and travis-ci api */
   var ownerAndProject = window.document.querySelectorAll("h1.public > strong > a")[0].pathname;
   var jsonPath = 'https://api.travis-ci.org/repositories' + ownerAndProject + '/builds.json';
@@ -29,51 +28,61 @@ if (overallDiv.length !== 0) {
   var buildTime = [];
   var buildColor = [];
   var allColor = ['#39aa56', '#db4545', '#f1e05a'];
+  var buildInfo = [];
   $.getJSON(jsonPath, function(data) {
-    console.log(data);
-    for (var i = 0; i < 10; i++) {
-      buildNum.push("#" + data[i]["number"]);
-      buildTime.push(Math.round(data[i]["duration"]/60*100)/100);
-      if (data[i]["state"] !== "finished") {
-          buildColor.push(allColor[2]);
-      } else {
-          buildColor.push(allColor[data[i]["result"]]);
-      }
-    }
-
-    var chart = new CanvasJS.Chart("chartContainer",
-    {
-      animationEnabled: true,
-      title:{
-        fontFamily: "Helvetica, arial, nimbussansl, liberationsans, freesans, clean, sans-serif",
-        text: "Travis-CI build Status (Recent 10 times)"
-      },
-      axisY: {
-        title: "Build Time(Minutes)"
-      },
-      axisX: {
-        title: "Build Number",
-        labelFontWeight: "bold"
-      },
-      data: [
-        {
-          type: "column", //change type to bar, line, area, pie, etc
-          dataPoints: [
-            { label: buildNum[9], y: buildTime[9], color: buildColor[9], toolTipContent:"<a href='http://baidu.com'>Go to this build</> <hr/> Build Time: 2min30s" },
-            { label: buildNum[8], y: buildTime[8], color: buildColor[8] },
-            { label: buildNum[7], y: buildTime[7], color: buildColor[7] },
-            { label: buildNum[6], y: buildTime[6], color: buildColor[6] },
-            { label: buildNum[5], y: buildTime[5], color: buildColor[5] },
-            { label: buildNum[4], y: buildTime[4], color: buildColor[4] },
-            { label: buildNum[3], y: buildTime[3], color: buildColor[3] },
-            { label: buildNum[2], y: buildTime[2], color: buildColor[2] },
-            { label: buildNum[1], y: buildTime[1], color: buildColor[1] },
-            { label: "Current:" + buildNum[0],  y: buildTime[0], color: buildColor[0] }
-          ]
+    if (data.length > 1) {
+      overallDiv[0].parentNode.insertBefore(chartDiv, overallDiv[0].nextSibling);
+      for (var i = 0; i < 10; i++) {
+        buildNum.push("#" + data[i]["number"]);
+        buildTime.push(Math.round(data[i]["duration"]/60*100)/100);
+        if (data[i]["state"] !== "finished") {
+            buildColor.push(allColor[2]);
+        } else {
+          var colorOrder = data[i]["result"];
+          if (colorOrder === null) {
+            colorOrder = 1;
+          }
+            buildColor.push(allColor[colorOrder]);
         }
-      ]
-    });
-    chart.render();
+        var buildId = data[i]["id"];
+        var buildTimeStr = Math.floor(data[i]["duration"]/60) + "min" + data[i]["duration"]%60 + "s";
+        buildInfo.push("<a href='https://travis-ci.org/Yaowenjie/yaowenjie.github.io/builds/" + buildId + "'>Go to this build</> <Br/> Build Time: " + buildTimeStr);
+      }
+
+      var chart = new CanvasJS.Chart("chartContainer",
+      {
+        animationEnabled: true,
+        title:{
+          fontFamily: "Helvetica, arial, nimbussansl, liberationsans, freesans, clean, sans-serif",
+          text: "Travis-CI build Status (Recent 10 times)"
+        },
+        axisY: {
+          title: "Build Time(Minutes)"
+        },
+        axisX: {
+          title: "Build Number",
+          labelFontWeight: "bold"
+        },
+        data: [
+          {
+            type: "column", //change type to bar, line, area, pie, etc
+            dataPoints: [
+              { label: buildNum[9], y: buildTime[9], color: buildColor[9], toolTipContent: buildInfo[9] },
+              { label: buildNum[8], y: buildTime[8], color: buildColor[8], toolTipContent: buildInfo[8] },
+              { label: buildNum[7], y: buildTime[7], color: buildColor[7], toolTipContent: buildInfo[7] },
+              { label: buildNum[6], y: buildTime[6], color: buildColor[6], toolTipContent: buildInfo[6] },
+              { label: buildNum[5], y: buildTime[5], color: buildColor[5], toolTipContent: buildInfo[5] },
+              { label: buildNum[4], y: buildTime[4], color: buildColor[4], toolTipContent: buildInfo[4] },
+              { label: buildNum[3], y: buildTime[3], color: buildColor[3], toolTipContent: buildInfo[3] },
+              { label: buildNum[2], y: buildTime[2], color: buildColor[2], toolTipContent: buildInfo[2] },
+              { label: buildNum[1], y: buildTime[1], color: buildColor[1], toolTipContent: buildInfo[1] },
+              { label: "Current:" + buildNum[0],  y: buildTime[0], color: buildColor[0], toolTipContent: buildInfo[0] }
+            ]
+          }
+        ]
+      });
+      chart.render();
+    }
   });
 
 
