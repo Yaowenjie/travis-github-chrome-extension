922bcc536f8dd25e63ab8ded734cd00652506d51
Yaowenjie <wjyao@thoughtworks.com>
2016-07-06 11:11:51 +0800

    Use correct color

--- chart.js
+++ chart.js
@@ -27,11 +27,18 @@ if (overallDiv.length !== 0) {
   console.log("------(7)" + jsonPath);
   var buildNum = [];
   var buildTime = [];
+  var buildColor = [];
+  var allColor = ['#39aa56', '#db4545', '#f1e05a'];
   $.getJSON(jsonPath, function(data) {
     console.log(data);
     for (var i = 0; i < 10; i++) {
       buildNum.push("#" + data[i]["number"]);
       buildTime.push(Math.round(data[i]["duration"]/60*100)/100);
+      if (data[i]["state"] !== "finished") {
+          buildColor.push(allColor[2]);
+      } else {
+          buildColor.push(allColor[data[i]["result"]]);
+      }
     }
 
     var chart = new CanvasJS.Chart("chartContainer",
@@ -52,16 +59,16 @@ if (overallDiv.length !== 0) {
         {
           type: "column", //change type to bar, line, area, pie, etc
           dataPoints: [
-            { label: buildNum[9], y: buildTime[9], color:'#39aa56', toolTipContent:"<a href='http://baidu.com'>Go to this build</> <hr/> Build Time: 2min30s" },
-            { label: buildNum[8], y: buildTime[8], color:'#39aa56' },
-            { label: buildNum[7], y: buildTime[7], color:'#db4545' },
-            { label: buildNum[6], y: buildTime[6], color:'#db4545' },
-            { label: buildNum[5], y: buildTime[5], color:'#39aa56' },
-            { label: buildNum[4], y: buildTime[4], color:'#39aa56' },
-            { label: buildNum[3], y: buildTime[3], color:'#39aa56' },
-            { label: buildNum[2], y: buildTime[2], color:'#39aa56' },
-            { label: buildNum[1], y: buildTime[1], color:'#39aa56' },
-            { label: "Current:" + buildNum[0],  y: buildTime[0], color:'#f1e05a' }
+            { label: buildNum[9], y: buildTime[9], color: buildColor[9], toolTipContent:"<a href='http://baidu.com'>Go to this build</> <hr/> Build Time: 2min30s" },
+            { label: buildNum[8], y: buildTime[8], color: buildColor[8] },
+            { label: buildNum[7], y: buildTime[7], color: buildColor[7] },
+            { label: buildNum[6], y: buildTime[6], color: buildColor[6] },
+            { label: buildNum[5], y: buildTime[5], color: buildColor[5] },
+            { label: buildNum[4], y: buildTime[4], color: buildColor[4] },
+            { label: buildNum[3], y: buildTime[3], color: buildColor[3] },
+            { label: buildNum[2], y: buildTime[2], color: buildColor[2] },
+            { label: buildNum[1], y: buildTime[1], color: buildColor[1] },
+            { label: "Current:" + buildNum[0],  y: buildTime[0], color: buildColor[0] }
           ]
         }
       ]
