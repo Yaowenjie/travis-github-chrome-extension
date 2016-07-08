992575ffbe1713eb644978131dcba5c824913d5b
Yaowenjie <wjyao@thoughtworks.com>
2016-07-08 23:25:11 +0800

    Change click behavior and cursor

--- chart.js
+++ chart.js
@@ -41,21 +41,26 @@ if (overallDiv.length !== 0) {
           {
             type: "column", //change type to bar, line, area, pie, etc
             dataPoints: [
-              { label: info.buildNum[9], y: info.buildTime[9], color: info.buildColor[9], toolTipContent: info.buildInfo[9] },
-              { label: info.buildNum[8], y: info.buildTime[8], color: info.buildColor[8], toolTipContent: info.buildInfo[8] },
-              { label: info.buildNum[7], y: info.buildTime[7], color: info.buildColor[7], toolTipContent: info.buildInfo[7] },
-              { label: info.buildNum[6], y: info.buildTime[6], color: info.buildColor[6], toolTipContent: info.buildInfo[6] },
-              { label: info.buildNum[5], y: info.buildTime[5], color: info.buildColor[5], toolTipContent: info.buildInfo[5] },
-              { label: info.buildNum[4], y: info.buildTime[4], color: info.buildColor[4], toolTipContent: info.buildInfo[4] },
-              { label: info.buildNum[3], y: info.buildTime[3], color: info.buildColor[3], toolTipContent: info.buildInfo[3] },
-              { label: info.buildNum[2], y: info.buildTime[2], color: info.buildColor[2], toolTipContent: info.buildInfo[2] },
-              { label: info.buildNum[1], y: info.buildTime[1], color: info.buildColor[1], toolTipContent: info.buildInfo[1] },
-              { label: "Current:" + info.buildNum[0],  y: info.buildTime[0], color: info.buildColor[0], toolTipContent: info.buildInfo[0] }
+              { label: info.buildNum[9], y: info.buildTime[9], color: info.buildColor[9], toolTipContent: info.buildInfo[9], click: onClick, cursor: "pointer" },
+              { label: info.buildNum[8], y: info.buildTime[8], color: info.buildColor[8], toolTipContent: info.buildInfo[8], click: onClick, cursor: "pointer" },
+              { label: info.buildNum[7], y: info.buildTime[7], color: info.buildColor[7], toolTipContent: info.buildInfo[7], click: onClick, cursor: "pointer" },
+              { label: info.buildNum[6], y: info.buildTime[6], color: info.buildColor[6], toolTipContent: info.buildInfo[6], click: onClick, cursor: "pointer"},
+              { label: info.buildNum[5], y: info.buildTime[5], color: info.buildColor[5], toolTipContent: info.buildInfo[5], click: onClick, cursor: "pointer" },
+              { label: info.buildNum[4], y: info.buildTime[4], color: info.buildColor[4], toolTipContent: info.buildInfo[4], click: onClick, cursor: "pointer" },
+              { label: info.buildNum[3], y: info.buildTime[3], color: info.buildColor[3], toolTipContent: info.buildInfo[3], click: onClick, cursor: "pointer" },
+              { label: info.buildNum[2], y: info.buildTime[2], color: info.buildColor[2], toolTipContent: info.buildInfo[2], click: onClick, cursor: "pointer" },
+              { label: info.buildNum[1], y: info.buildTime[1], color: info.buildColor[1], toolTipContent: info.buildInfo[1], click: onClick, cursor: "pointer" },
+              { label: "Current:" + info.buildNum[0],  y: info.buildTime[0], color: info.buildColor[0], toolTipContent: info.buildInfo[0], click: onClick, cursor: "pointer" }
             ]
           }
         ]
       });
       chart.render();
+
+      function onClick(e) {
+        var order = 9 - e.dataPoint.x;
+        window.open('https://travis-ci.org' + ownerAndProject + '/builds/' + data[order]["id"], '_blank');
+      }
     }
   });
 
@@ -64,16 +69,18 @@ if (overallDiv.length !== 0) {
     var buildTime = [];
     var buildColor = [];
     var buildInfo = [];
+    var info = {};
 
     for (var i = 0; i < range; i++) {
       var buildId = data[i]["id"];
       var buildDuration = data[i]["duration"];
       var buildState = data[i]["state"];
       var buildResult = data[i]["result"];
+      var buildMessage = data[i]["message"];
       var buildTimeStr = Math.floor(buildDuration/60) + "min" + buildDuration%60 + "s";
 
       buildNum.push("#" + data[i]["number"]);
-      buildInfo.push("<a href='https://travis-ci.org" + ownerAndProject + "/builds/" + buildId + "'>Go to this build</> <Br/> Build Time: " + buildTimeStr);
+      buildInfo.push("Build Time: " + buildTimeStr + "<br/><span>" + buildMessage + "</span>");
       buildTime.push(Math.round(buildDuration/60*100)/100);
 
       if (buildState !== "finished") {
@@ -86,10 +93,9 @@ if (overallDiv.length !== 0) {
     /* the current building is running*/
     if (buildTime[0] === 0) {
       buildTime[0] = (buildTime.length === 1) ? 1 : buildTime[1];
-      buildInfo[0] = "<a href='https://travis-ci.org" + ownerAndProject + "/builds/" + data[0]["id"] + "'>Go to this build <br/> It's Running!</a>";
+      buildInfo[0] = "It's Running!" + buildTimeStr + "<br/><span>" + buildMessage + "</span>";
     }
 
-    var info = {};
     info.buildNum = buildNum;
     info.buildTime = buildTime;
     info.buildColor = buildColor;
