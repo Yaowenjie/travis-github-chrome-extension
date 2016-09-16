109558b9a8d3a5d83db5d5f06e196bfaf052eeb3
Yaowenjie <wjyao@thoughtworks.com>
2016-09-16 22:41:09 +0800

    [#6] Redefine the position of status icon

--- status.js
+++ status.js
@@ -6,28 +6,26 @@ document.styleSheets[0].insertRule('.travis-ci img{display:block;}', 1);
 var selectors =
     [ 'h1.public > strong > a'
     , '.js-repo-list h3.f4 a:first-child'
-    , '.js-pinned-repos-reorder-container a.mb-2'
     ];
+var repoListSelectors = '.js-pinned-repos-reorder-container p.mb-0';
 
 selectors.forEach(function(sel) {
     $(sel).each(function() {
-        insertStatusIcon(this);
+        insertStatusIcon(this, this.pathname);
     });
 });
 
-function insertStatusIcon(el) {
-    var project = getProject(el);
+$(repoListSelectors).each(function() {
+  var project = $(this).siblings('span').children('a').attr('href');
+  insertStatusIcon(this, project);
+});
 
+function insertStatusIcon(el, project) {
     var img = $("<img src='https://api.travis-ci.org" + project + ".svg' alt='build status'></img>");
 
     img.load(function() {
         var link = $("<a class='travis-ci' href='https://travis-ci.org" + project + "'></a>");
         link.append(img);
-        link.insertAfter($(el));
+        link.appendTo($(el));
     });
-}
-
-function getProject(el) {
-  var project = el.pathname;
-  return project;
 }
