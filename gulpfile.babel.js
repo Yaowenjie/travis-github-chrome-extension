b530a1334d6a384e23479a35cb48332a67925f9b
Yaowenjie <wjyao@thoughtworks.com>
2017-02-14 23:41:46 +0800

    #15: remove gulp-watch, and trim gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,29 +2,41 @@ import gulp from 'gulp';
 import zip from 'gulp-zip';
 import flatten from 'gulp-flatten';
 import babel from 'gulp-babel';
-import watch from 'gulp-watch';
+import clean from 'gulp-clean';
 
 const DIST_PATH = "dist";
 const DEST_PATH = "builds";
 
-gulp.task("default", ["babel-transform"], () => {
-  return gulp.src(["src/img/*", "src/js/lib/*", "src/*"], {base: "."})
-    .pipe(flatten())
-    .pipe(gulp.dest(DIST_PATH));
+gulp.task('default', ['clean'], () => {
+  gulp.start('copy', 'babel-transform')
 });
 
-gulp.task("babel-transform", () => {
-  return watch('src/js/*.js', () => {
-    gulp.src(["src/js/*.js"], {base: "."})
-      .pipe(babel())
-      .pipe(flatten())
-      .pipe(gulp.dest(DIST_PATH));
-  });
+gulp.task('watch', () => {
+  gulp.watch('src/js/*.js', ['babel-transform']);
+  gulp.watch(["src/imgs/*", "src/js/lib/*", "src/*"], ['copy']);
 });
 
-gulp.task("build", () => {
+gulp.task('build', () => {
   return gulp.src(["src/**/*"], {base: "."})
     .pipe(flatten())
     .pipe(zip(`github-travis-chrome-extension.zip`))
     .pipe(gulp.dest(DEST_PATH));
+});
+
+gulp.task('babel-transform', () => {
+  return gulp.src(["src/js/*.js"], {base: "."})
+    .pipe(babel())
+    .pipe(flatten())
+    .pipe(gulp.dest(DIST_PATH));
+});
+
+gulp.task('copy', () => {
+  return gulp.src(["src/imgs/*", "src/js/lib/*", "src/*"], {base: "."})
+    .pipe(flatten())
+    .pipe(gulp.dest(DIST_PATH));
+});
+
+gulp.task('clean', () => {
+  return gulp.src(DIST_PATH, {read: false})
+    .pipe(clean());
 });
