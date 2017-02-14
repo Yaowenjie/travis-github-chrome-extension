1c767073e5ebd6924d87d0e73c8dd608ab69a8ef
Yaowenjie <wjyao@thoughtworks.com>
2017-02-14 21:09:01 +0800

    #15: restructure diretory and add default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp = require("gulp"),
-    zip = require("gulp-zip");
+    zip = require("gulp-zip"),
+    flatten = require('gulp-flatten');
 
+const DIST_PATH = "dist";
 const DEST_PATH = "builds";
 
+gulp.task("default", () => {
+  return gulp.src(["src/**/*"], {base: "."})
+    .pipe(flatten())
+    .pipe(gulp.dest(DIST_PATH));
+});
+
 gulp.task("build", () => {
-  return gulp.src(["src/*"],
-    {base: "."})
+  return gulp.src(["src/**/*"], {base: "."})
+    .pipe(flatten())
     .pipe(zip(`github-travis-chrome-extension.zip`))
-    .pipe(gulp.dest(DEST_PATH))
+    .pipe(gulp.dest(DEST_PATH));
 });
